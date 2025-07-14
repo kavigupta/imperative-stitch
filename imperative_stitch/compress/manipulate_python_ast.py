c6ea13fb8880438774be87e00b980c295737e73a
Kavi Gupta <kavig@mit.edu>
2025-07-14 15:07:43 -0400

    python with macros (#207)

--- imperative_stitch/compress/manipulate_python_ast.py
+++ imperative_stitch/compress/manipulate_python_ast.py
@@ -1,23 +1,53 @@
 import ast
+from dataclasses import dataclass
 
 import neurosym as ns
+from frozendict import frozendict
 
 
-def render_symvar(node):
+def render_symvar(node, *, is_pythonm):
     """
     Render this PythonAST as a __ref__ variable for stub display, i.e.,
         `a` -> `__ref__(a)`
     """
+    if is_pythonm:
+        return ns.make_python_ast.make_name(
+            ns.LeafAST(leaf=ns.PythonSymbol("&" + node.leaf.name, node.leaf.scope))
+        )
     return ns.make_python_ast.make_call(
         ns.PythonSymbol(name="__ref__", scope=None), ns.make_python_ast.make_name(node)
     )
 
 
-def render_codevar(node):
+@dataclass
+class QuotedCodeAST(ns.PythonAST):
+    content: ns.PythonAST
+
+    def to_python_ast(self):
+        """
+        Convert this QuotedCodeAST to a Python code object.
+        """
+        code_content = repr(self.content.to_python())
+        code_content = code_content[1:-1]
+        return ast.Name(
+            id=f"`{code_content}`",
+            kind=None,
+        )
+
+    def map(self, fn):
+        return QuotedCodeAST(fn(self.content.map(fn)))
+
+    def to_ns_s_exp(self, config=frozendict()):
+        return super().to_ns_s_exp(config)
+
+
+def render_codevar(node, *, is_pythonm):
     """
     Render this PythonAST as a __code__ variable for stub display, i.e.,
         `a` -> `__code__("a")`
     """
+    if is_pythonm:
+        return QuotedCodeAST(node)
     return ns.make_python_ast.make_call(
         ns.PythonSymbol(name="__code__", scope=None),
         ns.make_python_ast.make_constant(node.to_python()),
